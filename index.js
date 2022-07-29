e59d9d06ef8aa3d3dcd8af38b2896788956965d6
Mohammad Osman Goni <osmangoni0827@gmail.com>
2022-07-29 16:58:35 +0600

    update gitignore file

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-const bodyParser = require('body-parser');
 require('dotenv').config();
 const app = express()
-app.use(bodyParser.json())
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT||5200
